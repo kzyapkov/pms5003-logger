a402f962344bb64c9513745c2380d9b0d2a7e4bd
Kiril Zyapkov <kiril.zyapkov@gmail.com>
2017-03-30 09:33:05 +0300

    add setup.py, simplify data struct, accept arguments

--- pmlog.py
+++ pmlog.py
@@ -3,34 +3,49 @@ import sys
 import os
 import time
 import struct
+import logging
+import argparse
 from collections import namedtuple
 from threading import Event
-import logging
 
 from periphery import GPIO, Serial
 
-enable_gpio = 7
-reset_gpio = 19
-serial_port = '/dev/ttyS1'
+
+logging.basicConfig(level=logging.DEBUG, stream=sys.stdout,
+                    format='%(asctime)-15s %(levelname)-8s %(message)s')
+log = logging.getLogger()
+
+
+parser = argparse.ArgumentParser(description='PMS5003 data logger')
+parser.add_argument(
+        "-p", "--port", type=str, default="/dev/ttyS1",
+        help="Serial port connected to the PMS5003 sensor")
+parser.add_argument(
+        "--reset-pin", type=int, default=None,
+        help="GPIO number connected to the RESET signal")
+parser.add_argument(
+        "--enable-pin", type=int, default=None,
+        help="GPIO number connected to the SET (enable) signal")
+
 
 Packet = namedtuple('Packet', [
-        'pm1_std', 'pm25_std', 'pm10_st', 'pm01_atm', 'pm25_atm',
+        'pm1_std', 'pm25_std', 'pm10_std', 'pm01_atm', 'pm25_atm',
         'concentration_unit', 'count_03um', 'count_05um', 'count_1um',
-        'count_2_5u', 'count_5um', 'count_10um'])
+        'count_2_5um', 'count_5um', 'count_10um'])
 
 
 class PMS5003(object):
     def __init__(self, port, enable_pin=None, reset_pin=None):
-        self.port = port
+        self.port = Serial(port, 9600)
         self.gpio_enable = None
         self.gpio_reset = None
         self.stop = Event()
 
         # suspend sensor by default
-        if enable_pin is not None:
+        if enable_pin:
             self.gpio_enable = GPIO(enable_pin, "low")
 
-        if reset_pin is not None:
+        if reset_pin:
             self.gpio_reset = GPIO(reset_pin, "high")
 
     def reset(self):
@@ -43,9 +58,11 @@ class PMS5003(object):
         self.gpio_reset.write(True)
 
     def enable(self):
+        if not self.gpio_enable: return
         self.gpio_enable.write(True)
 
     def disable(self):
+        if not self.gpio_enable: return
         self.gpio_enable.write(False)
 
     @staticmethod
@@ -53,9 +70,7 @@ class PMS5003(object):
         numbers = struct.unpack('>16H', data)
         csum = sum(data[:-2])
         if csum != numbers[-1]:
-            #print(packet)
-            #print(csum)
-            #print(packet[-1])
+            log.warn("Bad packet data: %s / %s", data, csum)
             return
         return Packet(*numbers[2:-2])
 
@@ -78,19 +93,19 @@ class PMS5003(object):
 
 
 def main():
-    p = Serial(serial_port, 9600)
-    sensor = PMS5003(p, enable_gpio, reset_gpio)
+    args = parser.parse_args()
+    sensor = PMS5003(args.port, args.enable_pin, args.reset_pin)
     sensor.reset()
     start_at = time.time()
     try:
         while True:
             packet, now = sensor.receive_one(), time.time()
-            print("@{: 6.2f}\t{: 6d}\t{: 6d}\t{: 6d}".format(
+            log.info("@{: 6.2f}\t{: 6d}\t{: 6d}\t{: 6d}".format(
                     (now - start_at),
                     packet.pm01_atm, packet.pm25_atm, packet.concentration_unit))
     except KeyboardInterrupt:
         sensor.disable()
-        print("Bye bye.")
+        log.info("Bye bye.")
 
 if __name__ == "__main__":
     main()
