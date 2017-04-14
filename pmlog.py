9dec8407da99c86f2c95c5bc3d4dae5e3d1d4133
Kiril Zyapkov <kiril.zyapkov@gmail.com>
2017-04-14 10:33:34 +0300

    implement monitor and oneshot modes

--- pmlog.py
+++ pmlog.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import time
+import signal
 import struct
 import logging
 import argparse
@@ -11,7 +12,7 @@ from threading import Event
 from periphery import GPIO, Serial
 
 
-logging.basicConfig(level=logging.DEBUG, stream=sys.stdout,
+logging.basicConfig(level=logging.DEBUG, stream=sys.stderr,
                     format='%(asctime)-15s %(levelname)-8s %(message)s')
 log = logging.getLogger()
 
@@ -26,11 +27,24 @@ parser.add_argument(
 parser.add_argument(
         "--enable-pin", type=int, default=None,
         help="GPIO number connected to the SET (enable) signal")
+parser.add_argument(
+        "--warmup-time", type=int, default=30,
+        help="Seconds to wait before reading data")
+
+subparsers = parser.add_subparsers(dest="cmd")
+
+cmd_monitor_parser = subparsers.add_parser("monitor")
+
+cmd_monitor_parser.add_argument(
+        "--measure-period", type=int, default=60*5,
+        help="Seconds between measurements")
+
+cmd_oneshot_parser = subparsers.add_parser("oneshot")
 
 
 Packet = namedtuple('Packet', [
-        'pm1_std', 'pm25_std', 'pm10_std', 'pm01_atm', 'pm25_atm',
-        'concentration_unit', 'count_03um', 'count_05um', 'count_1um',
+        'pm1_std', 'pm25_std', 'pm10_std', 'pm01_atm', 'pm2_5_atm',
+        'pm10_atm', 'count_03um', 'count_05um', 'count_1um',
         'count_2_5um', 'count_5um', 'count_10um'])
 
 
@@ -59,11 +73,21 @@ class PMS5003(object):
 
     def enable(self):
         if not self.gpio_enable: return
+        log.info("Enable sensor (via gpio %s)", self.gpio_enable.pin)
         self.gpio_enable.write(True)
 
     def disable(self):
         if not self.gpio_enable: return
+        log.info("Disable sensor (via gpio %s)", self.gpio_enable.pin)
         self.gpio_enable.write(False)
+
+    def discard_input(self):
+        while self.port.input_waiting(): self.port.read(4096, 0)
+
+    def warmup(self, seconds):
+        log.info("Warming up for %s seconds", seconds)
+        self.stop.wait(seconds)
+        self.discard_input()
 
     @staticmethod
     def packet_from_data(data):
@@ -92,20 +116,56 @@ class PMS5003(object):
             if p: return p
 
 
+def run_monitor(sensor, args):
+    start_at = time.time()
+    sleep_period = args.measure_period - args.warmup_time
+    if args.warmup_time:
+        sensor.warmup(args.warmup_time)
+    try:
+        while not sensor.stop.is_set():
+            packet = sensor.receive_one()
+            if not packet: break
+            packet_at = time.time()
+            log.info("@{: 6.2f}\t{}".format((packet_at - start_at), packet))
+            if sleep_period > 0:
+                sensor.disable()
+                sensor.stop.wait(sleep_period)
+                if sensor.stop.is_set(): break
+                sensor.reset()
+                sensor.warmup(args.warmup_time)
+            else:
+                sensor.stop.wait(args.measure_period)
+    except KeyboardInterrupt:
+        log.info("Bye bye.")
+    finally:
+        sensor.disable()
+
+def run_oneshot(sensor, args):
+    if args.warmup_time:
+        sensor.warmup(args.warmup_time)
+    packet = sensor.receive_one()
+    log.info("{}".format(packet))
+    sensor.disable()
+
+def install_signal_handlers(sensor):
+    def _sighandler(signum, frame):
+        log.info("Got %s", signum)
+        sensor.stop.set()
+    signal.signal(signal.SIGINT, _sighandler)
+    signal.signal(signal.SIGTERM, _sighandler)
+
+
 def main():
     args = parser.parse_args()
     sensor = PMS5003(args.port, args.enable_pin, args.reset_pin)
     sensor.reset()
-    start_at = time.time()
-    try:
-        while True:
-            packet, now = sensor.receive_one(), time.time()
-            log.info("@{: 6.2f}\t{: 6d}\t{: 6d}\t{: 6d}".format(
-                    (now - start_at),
-                    packet.pm01_atm, packet.pm25_atm, packet.concentration_unit))
-    except KeyboardInterrupt:
-        sensor.disable()
-        log.info("Bye bye.")
+
+    install_signal_handlers(sensor)
+
+    if args.cmd == "monitor":
+        run_monitor(sensor, args)
+    elif args.cmd == "oneshot":
+        run_oneshot(sensor, args)
 
 if __name__ == "__main__":
     main()
