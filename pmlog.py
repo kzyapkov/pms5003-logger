dae426813fd57ad37601a62568d229e4cb2c64d3
Marcin Stojek <mstojek@gmail.com>
2017-08-10 21:47:24 +0200

    Images moved from code repository to wiki repository

--- pmlog.py
+++ pmlog.py
@@ -18,67 +18,66 @@ logging.basicConfig(level=logging.DEBUG, stream=sys.stderr,
                     format='%(asctime)-15s %(levelname)-8s %(message)s')
 log = logging.getLogger()
 
-
 parser = argparse.ArgumentParser(description='PMS5003 data logger')
 parser.add_argument(
-        "-p", "--port", type=str, default="/dev/ttyS1",
-        help="Serial port connected to the PMS5003 sensor")
+    "-p", "--port", type=str, default="/dev/ttyS1",
+    help="Serial port connected to the PMS5003 sensor")
 parser.add_argument(
-        "--reset-pin", type=int, default=None,
-        help="GPIO number connected to the RESET signal")
+    "--reset-pin", type=int, default=None,
+    help="GPIO number connected to the RESET signal")
 parser.add_argument(
-        "--enable-pin", type=int, default=None,
-        help="GPIO number connected to the SET (enable) signal")
+    "--enable-pin", type=int, default=None,
+    help="GPIO number connected to the SET (enable) signal")
 parser.add_argument(
-        "--warmup-time", type=int, default=30,
-        help="Seconds to wait before reading data")
+    "--warmup-time", type=int, default=30,
+    help="Seconds to wait before reading data")
 
 subparsers = parser.add_subparsers(dest="cmd")
 
 cmd_monitor_parser = subparsers.add_parser("monitor")
 
 cmd_monitor_parser.add_argument(
-        "--measure-period", type=int, default=60*5,
-        help="Seconds between measurements")
+    "--measure-period", type=int, default=60 * 5,
+    help="Seconds between measurements")
 
 cmd_oneshot_parser = subparsers.add_parser("oneshot")
 
 cmd_domoticz_parser = subparsers.add_parser("domoticz")
 cmd_domoticz_parser.add_argument(
-        "-ip", "--ip_address", required=True,
-        help="IP address of domoticz server")
+    "-ip", "--ip_address", required=True,
+    help="IP address of domoticz server")
 cmd_domoticz_parser.add_argument(
-        "-p", "--port", default=8080,
-        help="Port of domoticz server")
+    "-p", "--port", default=8080,
+    help="Port of domoticz server")
 cmd_domoticz_parser.add_argument(
-        "-m", "--mode", default='oneshot', choices=['oneshot', 'monitor'],
-        help="Monitor or oneshot mode")
+    "-m", "--mode", default='oneshot', choices=['oneshot', 'monitor'],
+    help="Monitor or oneshot mode")
 cmd_domoticz_parser.add_argument(
-        "--measure-period", type=int, default=60*5,
-        help="Seconds between measurements")
+    "--measure-period", type=int, default=60 * 5,
+    help="Seconds between measurements")
 cmd_domoticz_parser.add_argument(
-        "--pm_1_idx",
-        help="IDX of PM1 - if empty nothing will be reported to domoticz")		
+    "--pm_1_idx",
+    help="IDX of PM1 - if empty nothing will be reported to domoticz")
 cmd_domoticz_parser.add_argument(
-        "--pm_25_idx",
-        help="IDX of PM2.5 - if empty nothing will be reported to domoticz")	
+    "--pm_25_idx",
+    help="IDX of PM2.5 - if empty nothing will be reported to domoticz")
 cmd_domoticz_parser.add_argument(
-        "--pm_10_idx",
-        help="IDX of PM10 - if empty nothing will be reported to domoticz")
+    "--pm_10_idx",
+    help="IDX of PM10 - if empty nothing will be reported to domoticz")
 cmd_domoticz_parser.add_argument(
-        "--pm_1_percent_idx",
-        help="IDX of PM1 percent (100%% is 25 ug/m3) - if empty nothing will be reported to domoticz")		
+    "--pm_1_percent_idx",
+    help="IDX of PM1 percent (100%% is 25 ug/m3) - if empty nothing will be reported to domoticz")
 cmd_domoticz_parser.add_argument(
-        "--pm_25_percent_idx",
-        help="IDX of PM2.5 percent (100%% is 25 ug/m3) - if empty nothing will be reported to domoticz")	
+    "--pm_25_percent_idx",
+    help="IDX of PM2.5 percent (100%% is 25 ug/m3) - if empty nothing will be reported to domoticz")
 cmd_domoticz_parser.add_argument(
-        "--pm_10_percent_idx",
-        help="IDX of PM10 percent (100%% is 50 ug/m3) - if empty nothing will be reported to domoticz")		
-		
+    "--pm_10_percent_idx",
+    help="IDX of PM10 percent (100%% is 50 ug/m3) - if empty nothing will be reported to domoticz")
+
 Packet = namedtuple('Packet', [
-        'pm1_std', 'pm25_std', 'pm10_std', 'pm01_atm', 'pm2_5_atm',
-        'pm10_atm', 'count_03um', 'count_05um', 'count_1um',
-        'count_2_5um', 'count_5um', 'count_10um'])
+    'pm1_std', 'pm25_std', 'pm10_std', 'pm01_atm', 'pm2_5_atm',
+    'pm10_atm', 'count_03um', 'count_05um', 'count_1um',
+    'count_2_5um', 'count_5um', 'count_10um'])
 
 
 class PMS5003(object):
@@ -162,7 +161,7 @@ def run_monitor(sensor, args):
             if not packet: break
             packet_at = time.time()
             log.info("@{: 6.2f}\t{}".format((packet_at - start_at), packet))
-            if args.cmd == "domoticz" :
+            if args.cmd == "domoticz":
                 report_to_domoticz(packet, args)
             if sleep_period > 0:
                 sensor.disable()
@@ -178,6 +177,7 @@ def run_monitor(sensor, args):
     finally:
         sensor.disable()
 
+
 def run_oneshot(sensor, args):
     if args.enable_pin:
         sensor.enable()
@@ -186,39 +186,47 @@ def run_oneshot(sensor, args):
     try:
         packet = sensor.receive_one()
         log.info("{}".format(packet))
-        if args.cmd == "domoticz" :
+        if args.cmd == "domoticz":
             report_to_domoticz(packet, args)
     except KeyboardInterrupt:
-        log.info("Bye bye.")			
+        log.info("Bye bye.")
     finally:
         sensor.disable()
     sensor.disable()
+
 
 def install_signal_handlers(sensor):
     def _sighandler(signum, frame):
         log.info("Got %s", signum)
         sensor.stop.set()
+
     signal.signal(signal.SIGINT, _sighandler)
     signal.signal(signal.SIGTERM, _sighandler)
 
+
 def report_to_domoticz(packet, args):
-	
     if args.pm_1_idx:
-	    send_http_request_to_domoticz(ip=args.ip_address, port=args.port, idx=args.pm_1_idx, idx_value=packet.pm01_atm)
+        send_http_request_to_domoticz(ip=args.ip_address, port=args.port, idx=args.pm_1_idx, idx_value=packet.pm01_atm)
     if args.pm_25_idx:
-	    send_http_request_to_domoticz(ip=args.ip_address, port=args.port, idx=args.pm_25_idx, idx_value=packet.pm2_5_atm)
+        send_http_request_to_domoticz(ip=args.ip_address, port=args.port, idx=args.pm_25_idx,
+                                      idx_value=packet.pm2_5_atm)
     if args.pm_10_idx:
-	    send_http_request_to_domoticz(ip=args.ip_address, port=args.port, idx=args.pm_10_idx, idx_value=packet.pm10_atm)
+        send_http_request_to_domoticz(ip=args.ip_address, port=args.port, idx=args.pm_10_idx, idx_value=packet.pm10_atm)
     if args.pm_1_percent_idx:
-	    send_http_request_to_domoticz(ip=args.ip_address, port=args.port, idx=args.pm_1_percent_idx, idx_value=packet.pm01_atm*4)
+        send_http_request_to_domoticz(ip=args.ip_address, port=args.port, idx=args.pm_1_percent_idx,
+                                      idx_value=packet.pm01_atm * 4)
     if args.pm_25_percent_idx:
-	    send_http_request_to_domoticz(ip=args.ip_address, port=args.port, idx=args.pm_25_percent_idx, idx_value=packet.pm2_5_atm*4)
+        send_http_request_to_domoticz(ip=args.ip_address, port=args.port, idx=args.pm_25_percent_idx,
+                                      idx_value=packet.pm2_5_atm * 4)
     if args.pm_10_percent_idx:
-	    send_http_request_to_domoticz(ip=args.ip_address, port=args.port, idx=args.pm_10_percent_idx, idx_value=packet.pm10_atm*2)
-	
+        send_http_request_to_domoticz(ip=args.ip_address, port=args.port, idx=args.pm_10_percent_idx,
+                                      idx_value=packet.pm10_atm * 2)
+
+
 def send_http_request_to_domoticz(ip, port, idx, idx_value):
-    url = "http://" + ip + ":" + port +"/json.htm?type=command&param=udevice&nvalue=0&idx=" + str(idx) + "&svalue=" + str(idx_value)
-    #print(url)
+    url = "http://" + ip + ":" + port + "/json.htm?type=command&param=udevice&nvalue=0&idx=" + str(
+        idx) + "&svalue=" + str(idx_value)
+    # print(url)
     request = urllib2.Request(url)
     try:
         response = urllib2.urlopen(request)
@@ -226,11 +234,12 @@ def send_http_request_to_domoticz(ip, port, idx, idx_value):
         log.info('HTTPError = ' + str(e.code))
     except urllib2.URLError, e:
         log.info('URLError = ' + str(e.reason))
-    #except httplib.HTTPException, e:
+    # except httplib.HTTPException, e:
     #    log.info('HTTPException')
     except Exception:
         import traceback
-        log.info('generic exception: ' + traceback.format_exc())	
+        log.info('generic exception: ' + traceback.format_exc())
+
 
 def main():
     args = parser.parse_args()
@@ -248,6 +257,7 @@ def main():
             run_monitor(sensor, args)
         elif args.mode == "oneshot":
             run_oneshot(sensor, args)
+
 
 if __name__ == "__main__":
     main()
